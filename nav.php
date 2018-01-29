d796c1f60d67e221f86d69007a21141aafba0ae0
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-01-29 15:28:14 +0100

    beta.2

---
+++ nav.php
@@ -0,0 +1,36 @@
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md">
+            <div class="container"><a class="navbar-brand" href="index.php">Bts SIO</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                    <form class="form-inline navbar-left">
+                        <div class="input-group"><span id="basic-addon1" class="input-group-addon"><i class="fa fa-search"></i></span><input class="form-control" type="text" placeholder="Search" aria-describedby="basic-addon1"></div>
+                    </form>
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php">Accueil </a></li>
+                        <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Composent </a>
+                            <div class="dropdown-menu" role="menu">
+                              <a class="dropdown-item" role="presentation" href="#">Ordinateur</a>
+                              <a class="dropdown-item" role="presentation" href="#">Impriman</a>
+                              <a class="dropdown-item" role="presentation" href="#">Third Item</a>
+                            </div>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Assistance</a>
+                            <div class="dropdown-menu" role="menu">
+                              <a class="dropdown-item" role="presentation" href="#">Tickets</a>
+                              <a class="dropdown-item" role="presentation" href="#">Créer un ticket</a>
+                              <a class="dropdown-item" role="presentation" href="#">Third Item</a>
+                            </div>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Administrateur</a>
+                            <div class="dropdown-menu" role="menu">
+                              <a class="dropdown-item" role="presentation" href="#">Utilisateur</a>
+                              <a class="dropdown-item" role="presentation" href="#">Groupe</a>
+                              <a class="dropdown-item" role="presentation" href="#">Profils</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
