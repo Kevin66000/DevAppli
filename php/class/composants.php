e14d00f3a6bec2099fae9c75b8fecf001258bdec
charles66820 <charles.goedefroit@gmail.com>
2018-03-24 16:34:24 +0100

    class

---
+++ php/class/composants.php
@@ -0,0 +1,70 @@
+<?php
+
+  class Composant {
+    private $nomCompo;
+    private $lieu;
+    private $statut;
+    private $gabarit;
+    private $type;
+    private $fabricant;
+    private $modele;
+    private $numSerie;
+    private $addrMac;
+    private $referenceCompo;
+
+    function __construct() {
+      this->nomCompo;
+      this->lieu;
+      this->statut;
+      this->gabarit;
+      this->type;
+      this->fabricant;
+      this->modele;
+      this->numSerie;
+      this->addrMac;
+      this->referenceCompo;
+    }
+
+    function getNomCompo() {
+      return this->nomCompo;
+    }
+
+    function setNomCompo($pNomCompo) {
+      this->nomCompo = $pNomCompo;
+    }
+
+    function getLieu() {
+      return this->lieu;
+    }
+
+    function setLieu($pLieu) {
+      this->lieu = $pLieu;
+    }
+
+    function getStatut() {
+      return this->statut;
+    }
+
+    function setStatut($pStatut) {
+      this->statut = $pStatut
+    }
+
+    function getGabarit() {
+      return this->gabarit;
+    }
+
+    function setGabarit
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+ ?>
