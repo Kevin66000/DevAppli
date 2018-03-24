aeddce195726ab1705d6f4105316bc411d655921
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-03-24 17:43:50 +0100

    Merge branch 'master' of https://github.com/Kevin66000/DevAppli

--- php/class/composant.php
+++ php/class/composant.php
@@ -13,77 +13,77 @@
     private $referenceCompo;
 
     function __construct() {
-      this->nomCompo;
-      this->lieu;
-      this->statut;
-      this->gabarit;
-      this->type;
-      this->fabricant;
-      this->modele;
-      this->numSerie;
-      this->addrMac;
-      this->referenceCompo;
+      $this->nomCompo;
+      $this->lieu;
+      $this->statut;
+      $this->gabarit;
+      $this->type;
+      $this->fabricant;
+      $this->modele;
+      $this->numSerie;
+      $this->addrMac;
+      $this->referenceCompo;
     }
 
     function getNomCompo() {
-      return this->nomCompo;
+      return $this->nomCompo;
     }
     function setNomCompo($pNomCompo) {
-      this->nomCompo = $pNomCompo;
+      $this->nomCompo = $pNomCompo;
     }
     function getLieu() {
-      return this->lieu;
+      return $this->lieu;
     }
     function setLieu($pLieu) {
-      this->lieu = $pLieu;
+      $this->lieu = $pLieu;
     }
     function getStatut() {
-      return this->statut;
+      return $this->statut;
     }
     function setStatut($pStatut) {
-      this->statut = $pStatut
+      $this->statut = $pStatut
     }
     function getGabarit() {
-      return this->gabarit;
+      return $this->gabarit;
     }
     function setGabarit($pGabarit) {
-      this->gabarit = $pGabarit;
+      $this->gabarit = $pGabarit;
     }
     function getType() {
-      return this->type;
+      return $this->type;
     }
     function setType($pType) {
-      this->type = $pType;
+      $this->type = $pType;
     }
     function getFabricant() {
-      return this->fabricant;
+      return $this->fabricant;
     }
     function setType($pFabricant) {
-      this->fabricant = $pFabricant;
+      $this->fabricant = $pFabricant;
     }
     function getModele() {
-      return this->modele;
+      return $this->modele;
     }
     function setModele($pModele) {
-      this->modele = $pModele;
+      $this->modele = $pModele;
     }
     function getNumSerie() {
-      return this->numSerie;
+      return $this->numSerie;
     }
     function setNumSerie($pNumSerie) {
-      this->numSerie = $pNumSerie;
+      $this->numSerie = $pNumSerie;
     }
     function getAddrMac() {
-      return this->addrMac;
+      return $this->addrMac;
     }
     function setAddrMac($pAddrMac) {
-      this->addrMac = $pAddrMac;
+      $this->addrMac = $pAddrMac;
     }
     function getReferenceCompo() {
-      return this->referenceCompo;
+      return $this->referenceCompo;
     }
     function setReferenceCompo($pReferenceCompo) {
-      this->referenceCompo = $pReferenceCompo;
+      $this->referenceCompo = $pReferenceCompo;
     }
 }
 
