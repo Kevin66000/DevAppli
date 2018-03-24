930c04f107e24134ed44ba3610491328a611c55a
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-03-24 15:50:48 +0100

    compo

--- php/nav.php
+++ php/nav.php
@@ -4,9 +4,6 @@
     <div class="container"><a class="navbar-brand" href="accueil.php">Bts SIO</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse"
       id="navcol-1">
-      <form class="form-inline navbar-left">
-        <div class="input-group"><span id="basic-addon1" class="input-group-addon"><i class="fa fa-search"></i></span><input class="form-control" type="text" placeholder="Search" aria-describedby="basic-addon1"></div>
-      </form>
       <ul class="nav navbar-nav ml-auto">
         <li class="nav-item" role="presentation"><a class="nav-link active" href="accueil.php">Accueil </a></li>
         <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Composent </a>
