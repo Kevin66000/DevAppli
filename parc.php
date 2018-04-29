537c6069e5b3105d97e99d63398aa1d14819091c
charles66820 <charles.goedefroit@gmail.com>
2018-04-30 00:13:48 +0200

    parc fini

--- parc.php
+++ parc.php
@@ -12,8 +12,79 @@
     //test si l'utilisateur est connecter
     if (isset($_SESSION['idUser'])) {
       ?>
-
-
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 border border-dark">
+            <fieldset>
+              <legend></legend>
+              <div class="col-12 col-md">
+                <h2></h2>
+                <ul class="list-group" style="overflow:auto; height: 500px;" >
+                </ul>
+              </div>
+            </fieldset>
+          </div>
+          <div class="col-12 col-md-6 border border-dark">
+            <fieldset>
+              <legend>salle i109</legend>
+              <div class="container">
+                <h2>Les machines</h2>
+                <ul class="list-group" style="overflow:auto; height: 500px;" >
+                  <?php
+                  $reqcompoparc = $bdd->prepare("SELECT parcComposants.nomComposants, parcComposants.typeComposants FROM parcComposants, salle WHERE parcComposants.idSalle = salle.idSalle AND salle.libelleSalle = ?");
+                  $reqcompoparc->execute(array("i109"));
+                  foreach ($reqcompoparc->fetchAll() as $row) {
+                    ?>
+                    <li class="list-group-item"><?php echo $row['nomComposants']." | ".$row['typeComposants'] ?></li>
+                    <?php
+                  }
+                  ?>
+                </ul>
+              </div>
+            </fieldset>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-md-6 border border-dark">
+            <fieldset>
+              <legend>salle i110</legend>
+              <div class="col-12 col-md">
+                <h2>Les machines</h2>
+                <ul class="list-group" style="overflow:auto; height: 500px;" >
+                  <?php
+                  $reqcompoparc = $bdd->prepare("SELECT parcComposants.nomComposants, parcComposants.typeComposants FROM parcComposants, salle WHERE parcComposants.idSalle = salle.idSalle AND salle.libelleSalle = ?");
+                  $reqcompoparc->execute(array("i110"));
+                  foreach ($reqcompoparc->fetchAll() as $row) {
+                    ?>
+                    <li class="list-group-item"><?php echo $row['nomComposants']." | ".$row['typeComposants'] ?></li>
+                    <?php
+                  }
+                  ?>
+                </ul>
+              </div>
+            </fieldset>
+          </div>
+          <div class="col-12 col-md-6 border border-dark">
+            <fieldset>
+              <legend>salle i111</legend>
+              <div class="container">
+                <h2>Les machines</h2>
+                <ul class="list-group" style="overflow:auto; height: 500px;" >
+                  <?php
+                  $reqcompoparc = $bdd->prepare("SELECT parcComposants.nomComposants, parcComposants.typeComposants FROM parcComposants, salle WHERE parcComposants.idSalle = salle.idSalle AND salle.libelleSalle = ?");
+                  $reqcompoparc->execute(array("i111"));
+                  foreach ($reqcompoparc->fetchAll() as $row) {
+                    ?>
+                    <li class="list-group-item"><?php echo $row['nomComposants']." | ".$row['typeComposants'] ?></li>
+                    <?php
+                  }
+                  ?>
+                </ul>
+              </div>
+            </fieldset>
+          </div>
+        </div>
+      </div>
       <?php
     }else {
       header("Location: /");//redirige ver la pages d'accueil
